fbc9de58b51a1cd8b237eb192ba74558d0ead0ec
Void04 <xiayuxuan@live.com>
2021-01-27 00:29:05 +0800

    migrated from old presets

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-app>
+    <v-app-bar dark app>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>公开招募计算器</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="this.$store.commit('resetTags')"
+        ><v-icon>mdi-autorenew</v-icon></v-btn
+      >
+      <v-btn icon @click="showTags = !showTags"
+        ><v-icon>{{ collapse }}</v-icon></v-btn
+      >
+    </v-app-bar>
+
+    <v-navigation-drawer temporary v-model="drawer" app>
+      <v-list-item>
+        <v-list-item two-line link href="https://ak.rua.best/">
+          <v-list-item-avatar tile size="40">
+            <v-img
+              src="https://i.loli.net/2021/01/23/cLBC8qba9uAtkVG.png"
+            ></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>明日方舟工具箱</v-list-item-title>
+            <v-list-item-subtitle>ak.rua.best</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav shaped>
+        <v-list-item v-for="(item, index) in navItems" :key="index" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content href="item.href">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <recruit :show-tags="showTags"></recruit>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Recruit from "./components/Recruit.vue";
 
 @Component({
-  components: {
-    HelloWorld
-  }
+  components: { Recruit }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  showTags = true;
+  drawer = null;
+  navItems = [
+    {
+      title: "公开招募计算器",
+      icon: "mdi-magnify",
+      href: "https://ak.rua.best/recruit"
+    },
+    {
+      title: "关于",
+      icon: "mdi-account-question",
+      href: "https://ak.rua.best/about"
+    }
+  ];
+  get collapse(): string {
+    return this.showTags
+      ? "mdi-unfold-less-horizontal"
+      : "mdi-unfold-more-horizontal";
+  }
+}
 </script>
 
 <style>
---
+++ src/components/Recruit.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container v-on:clear-tags="clearTags">
+    <v-slide-y-transition>
+      <v-col cols="12" sm="8" offset-sm="2" v-show="showTags">
+        <v-row v-for="(group, name, index) in tags" :key="index" cols="12">
+          <v-sheet class="my-2">
+            <h2 class="title">{{ name }}</h2>
+            <v-chip-group
+              v-model="selected"
+              multiple
+              column
+              justify="space-around"
+            >
+              <v-chip
+                v-for="(value, tag) in group"
+                :key="value"
+                :value="value"
+                filter
+                >{{ tag }}</v-chip
+              >
+            </v-chip-group>
+            <v-divider></v-divider>
+          </v-sheet>
+        </v-row>
+      </v-col>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+@Component
+export default class HelloWorld extends Vue {
+  @Prop({ default: true })
+  showTags!: boolean;
+  selected = [1];
+  tags = {
+    获得途径: { 公招可见: 1 },
+    职业: {
+      先锋: 2,
+      近卫: 4,
+      狙击: 8,
+      重装: 16,
+      医疗: 32,
+      辅助: 64,
+      术师: 128,
+      特种: 256
+    },
+    部署位: { 近战位: 512, 远程位: 1024 },
+    性别: { 男性干员: 2048, 女性干员: 4096, 断罪性干员: 8192 },
+    一般标签: {
+      治疗: 16384,
+      支援: 32768,
+      输出: 65536,
+      群攻: 131072,
+      减速: 262144,
+      生存: 524288,
+      防护: 1048576,
+      削弱: 2097152,
+      位移: 4194304,
+      控场: 8388608,
+      爆发: 16777216,
+      召唤: 33554432,
+      快速复活: 67108864,
+      费用回复: 134217728,
+      支援机械: 268435456,
+      新手: 536870912
+    }
+  };
+  clearTags(): void {
+    this.selected = [1];
+  }
+}
+</script>
