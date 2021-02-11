7f711caf0baea5f782e5df7c61b1752def8f05ae
Void04 <xiayuxuan@live.com>
2021-02-11 19:54:00 +0800

    use jsdelivr cdn

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/ark_toolbox/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "https://cdn.jsdelivr.net/gh/V04/ark_toolbox@latest/docs/"
+      : "/",
   configureWebpack: {
     devtool: "source-map"
   },
